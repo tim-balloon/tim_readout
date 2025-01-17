be99dd519785fd680f2f365283e2c766da9961da
James Burgoyne <thejaburjunk+github@gmail.com>
2025-01-17 09:30:26 -0800

    debug

--- scripts/startup_board.sh
+++ scripts/startup_board.sh
@@ -7,7 +7,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Navigate to the init script directory
-cd /user/xilinx/primecam_readout/scripts/
+cd /home/xilinx/primecam_readout/scripts/
 
 # Activate the environment
 source activate
