a00cd6956e77700fcbf6e3374de0252776d41af4
James Burgoyne <thejaburjunk+github@gmail.com>
2025-04-24 14:41:21 -0700

    debug

--- src/queen_commands/test_functions.py
+++ src/queen_commands/test_functions.py
@@ -62,8 +62,8 @@ def loopbackCapture():
 
     bid = 1
     drid = 1
-    N_packets = 4096 # 4096 samples ~ 8.4 s
-    # N_packets = 3
+    # N_packets = 4096 # 4096 samples ~ 8.4 s
+    N_packets = 10
 
     _sendCom(bid, drid, "setNCLO", 500)        # set LO
     _sendCom(bid, drid, "writeNewVnaComb")     # gen. tone comb
